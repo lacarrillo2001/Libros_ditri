8987e4d6b75d610abdf105f75c1b7539d8e3320d
lacarrillo2001 <lacarrillo6@espe.edu.ec>
2026-01-20 13:19:11 -0500

    fun

---
+++ oauth-server/oauth-server/src/main/java/com/espe/oauth_server/OauthServerApplication.java
@@ -0,0 +1,15 @@
+package com.espe.oauth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthServerApplication.class, args);
+    }
+
+    //HOLAMUNDO
+
+}
